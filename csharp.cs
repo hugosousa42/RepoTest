0474b068fe72b8c75a3bf3965d0b7ccc1703ab9f
Hugo Sousa <hugosousa42@proton.me>
2025-03-28 22:17:29 +0000

    asasas

--- csharp.cs
+++ csharp.cs
@@ -1,9 +1,9 @@
-using System;
-
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.ReadLine();  
     }
+        Console.WriteLine("Hello, test!");
 }
