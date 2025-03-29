9b2ade02f3d85b416fcebaf26910f1b2e4212cf9
Hugo Sousa <hugosousa42@proton.me>
2025-03-29 00:44:53 +0000

    TTTTTTTTTT

---
+++ csharp copy 2.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace.test
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        if(true)
+        {
+            Console.ReadLine("Hello.");
+        }
+    }
+}
