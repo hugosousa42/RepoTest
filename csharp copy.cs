47d9f8f4a4ccf96ff0510961734015a5f086d8cd
Hugo Sousa <hugosousa42@proton.me>
2025-03-29 00:12:30 +0000

    cs teste

--- csharp copy.cs
+++ csharp copy.cs
@@ -1,9 +1,9 @@
 public class HelloWorld
 {
     public static void Main(string[] args)
+    public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Console.ReadLine();  
     }
-        Console.WriteLine("Hello, test!");
 }
--- csharp.cs
+++ csharp.cs
@@ -7,7 +7,6 @@ public class HelloWorld
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, test!");
         Console.ReadLine();  
     }
 }
